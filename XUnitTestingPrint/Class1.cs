a39f578353178449ff6448066e11ddfeb89f9803
Mariano <mgenovese@iid.ec>
2017-02-10 11:05:01 -0500

    Add project files.

---
+++ XUnitTestingPrint/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace XUnitTestingPrint
+{
+    public class Class1
+    {
+		private readonly ITestOutputHelper output;
+
+		public Class1(ITestOutputHelper output)
+		{
+			this.output = output;
+		}
+
+		[Fact]
+		public void PassingTest()
+		{
+			this.output.WriteLine("Hola");
+			Assert.Equal(4, Add(2, 2));
+		}
+
+		[Fact]
+		public void FailingTest()
+		{
+			Assert.Equal(5, Add(2, 2));
+		}
+
+		int Add(int x, int y)
+		{
+			return x + y;
+		}
+	}
+}
